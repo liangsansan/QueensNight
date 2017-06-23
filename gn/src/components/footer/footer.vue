8aa99082a9417521514a16faeb3b99b58c075fc0
Yxc <289174785@qq.com>
2017-06-23 09:09:09 +0800

    123

--- gn/src/components/footer/footer.vue
+++ gn/src/components/footer/footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<ul class="foot">
 		<li><i class="iconfont icon-home"></i><span>首页</span></li>
-		<router-link to="/details"><li><i class="iconfont icon-xiao46"></i><span>商品分类</span></li></router-link>
+		<router-link to="/wenxiong"><li><i class="iconfont icon-xiao46"></i><span>商品分类</span></li></router-link>
 		<li><i class="iconfont icon-konggouwuche"></i><span>购物车</span></li>
 		<li><i class="iconfont icon-gerenzhongxin"></i><span>用户中心</span></li>
 	</ul>
