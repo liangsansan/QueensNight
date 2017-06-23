9e787484bb9f1e01f8de325276b659613ed80222
Yxc <289174785@qq.com>
2017-06-23 10:07:35 +0800

    123

--- gn/src/components/dzy_details/wenxiong/wenxiong.vue
+++ gn/src/components/dzy_details/wenxiong/wenxiong.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wenxiong_main">
-		<div class="w_m_box">
-			<div>5 
+		<div class="w_m_box" v-for=" item in res">
+			<div>{{item}}</div>
 		</div>
 	</div>
 </template>
@@ -15,15 +15,19 @@ import './wenxiong.scss'
 		},
 		data(){
 			return {
-				
+				res:[1,2,3,4,5]
 			}
 		},
 		methods: {
-
+			even: function (numbers) {
+    			return numbers.filter(function (number) {
+      				return number % 2 === 0
+    			})
+ 		 	}
 		},
 		created(){
 			$.get('./src/components/dzy_details/shop.json',function(res){
-				console.log(res);
+				console.log(this);
 			})
 		}
 	}
