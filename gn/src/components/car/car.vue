a4b525f34a689198e4478e21c713e783780dadb3
Lcc <529228684@qq.com>
2017-06-26 21:04:43 +0800

    Lcc:26号晚上

--- gn/src/components/car/car.vue
+++ gn/src/components/car/car.vue
@@ -5,41 +5,28 @@
             <p>购物车</p>
         </div>
         <div class="car_main">
-            <div class="car_msg">
+            <div class="car_msg" v-if="products.length">
                 购物车还没有商品哦<br><br>
                 <router-link to="/">先去逛逛吧</router-link>
             </div>
             <div class="car_show">
                 <div class="car_allcheck">
-                    <span class="all_check" @click="allcheck"></span>全选
+                    <span class="all_check" @click="selectAll" :class="{check_click:isSelectAll}"></span>全选
+                    <span class="unall_check" @click="unselectAll" v-if="isSelectAll">取消全选</span>
                 </div>
                 <div class="car_list">
-                    <ul>
-                        
-                        <li v-for="(item, index) in products[0]">
-                            <span class="check" @click="checked"></span>
+                    <ul id="list_content">
+                        <li v-for="(item, index) in products">
+                            <span class="check" @click="selectGood(item)" :class="{check_click:item.isChecked}"></span>
                             <img src="../../assets/imgs/01.jpg" alt="">
                             <span class="car_title">{{item.qnTitle}}</span>
                             <span class="car_color">颜色：{{item.qnColor[0]}}</span>
                             <span class="car_chima">尺码：{{item.qnSize[0]}}</span>
                             <span class="car_price">{{item.qnPrimaryPrices}}</span>
                             <div class="car_shuliang">
-                                <span class="jia iconfont icon-xiaojiantou_shang_mianxing-copy" @click="zeng"></span>
-                                <input type="text" class="car_count" v-model="qty">
-                                <span class="jian iconfont icon-xiaojiantou_xia_mianxing-copy" @click="shan"></span>
-                            </div>
-                        </li>
-                        <li v-for="(item, index) in products[0]">
-                            <span class="check" @click="checked"></span>
-                            <img src="../../assets/imgs/01.jpg" alt="">
-                            <span class="car_title">{{item.qnTitle}}</span>
-                            <span class="car_color">颜色：{{item.qnColor[0]}}</span>
-                            <span class="car_chima">尺码：{{item.qnSize[0]}}</span>
-                            <span class="car_price">{{item.qnPrimaryPrices}}</span>
-                            <div class="car_shuliang">
-                                <span class="jia iconfont icon-xiaojiantou_shang_mianxing-copy" @click="zeng"></span>
-                                <input type="text" class="car_count" v-model="qty">
-                                <span class="jian iconfont icon-xiaojiantou_xia_mianxing-copy" @click="shan"></span>
+                                <span class="jia iconfont icon-xiaojiantou_shang_mianxing-copy" @click="changeQuentity(item,1,index)"></span>
+                                <input type="text" class="car_count" :value="item.qnAmount" disabled>
+                                <span class="jian iconfont icon-xiaojiantou_xia_mianxing-copy" @click="changeQuentity(item,-1,index)"></span>
                             </div>
                         </li>
                     </ul>
@@ -52,9 +39,19 @@
                 <span class="total_msg">合计：</span>
                 <span class="total_price">{{totalPrice}}</span>
             </div>
-            <div class="delete" @click="Delete">删除</div>
+            <div class="delete" @click="ifDelete">删除</div>
             <div class="car_gopay">
                 去结算
+            </div>
+        </div>
+        <div class="car_window" v-if="askDelete">
+            <div class="ask">
+                <div><span class="ask_close">X</span></div>
+                <div class="ask_text" @click="notDelete">您确定要删除所选商品吗？</div>
+                <div class="ask_btn">
+                    <span class="ask_yes" @click="goDelete">确定</span>
+                    <span class="ask_no" @click="notDelete">不了</span>
+                </div>
             </div>
         </div>
     </div>
@@ -67,65 +64,109 @@
     import http from '../../utils/HttpClient'
 
     export default {
+        
         components:{
             
-        },
-        computed:{
-            totalPrice:function (){
-                var aa = $('.check[check]').next('.car_price').html()
-                console.log(aa)
-                return aa
-            }
         },
         data(){
             return{
                 products:[],
-                qty:12
+                isSelectAll:false,
+                askDelete:false
             }
         },
         methods:{
-            allcheck(){
-                if($('.all_check').hasClass('check_click')){
-                    $('.all_check').removeClass('check_click');
-                    $('.check').removeClass('check_click');
-                    $('.all_check').removeAttr("check");
-                    $('.check').removeAttr("check");
-                }else{
-                    $('.all_check').addClass('check_click');
-                    $('.check').addClass('check_click');
-                    $('.all_check').attr("check","checked");
-                    $('.check').attr("check","checked");
+            selectAll(){
+                this.isSelectAll = true;
+                this.products.forEach((good)=>{
+                    good.isChecked = true;
+                });
+		    },
+            unselectAll(){
+                this.isSelectAll = false;
+                this.products.forEach((good)=>{
+                    good.isChecked = false;
+                })
+		    },
+            selectGood(goodObj,index){
+                if(goodObj.isChecked == void 0){
+                    this.$set(goodObj,"isChecked",true)
+                } else {
+                    goodObj.isChecked = !goodObj.isChecked;
                 }
-            },
-            checked(event){
-                var self = $(event.target);
-                if(self.hasClass('check_click')){
-                    self.removeClass('check_click');
-                    self.removeAttr("check");
-                }else{
-                    self.addClass('check_click');
-                    self.attr("check","checked");
+                this.isCheckAll();
+		    },
+            isCheckAll(){
+                var flag = true;
+                this.products.forEach(function(good){
+                    if(!good.isChecked){
+                        flag = false;
+                    }
+                });
+                if(!flag){
+                    this.isSelectAll = false;
+                } else {
+                    this.isSelectAll = true;
                 }
+		    },
+            changeQuentity(good,val,_index){
+                if(good.qnAmount == 1 && val == -1 ){
+                    alert('商品数量不能小于1')
+                } else {
+                    var qty = Number(good.qnAmount)
+                    qty += val;
+                    good.qnAmount = qty	
+                    // var aa = document.getElementById("list_content")
+                    // var bb = aa.getElementsByTagName("*")
+                    // console.log(bb.length)
+                }
+		    },
+            goDelete(){
+                this.askDelete = false;
+                // 使用传统写法
+                // 遍历商品列表删除所有选中的
+                this.products.forEach(function(item,index,_list){
+                    if(item.isChecked){
+                        _list.splice(index,1);
+                    }
+                });
             },
-            zeng(event){
-                // console.log(event.target)
-                // var cal = $(event.target);
-                // let count = cal.next().val();
-                // count ++;
-                // console.log(count)
-                this.qty++
-                console.log($('.check'))
+            notDelete(){
+                this.askDelete = false;
             },
-            shan(){
-                // console.log(event.currentTarget)
-                // let aa = $('.check[check]').parent('li').detach()
-                // console.log(aa)
-                this.qty--
-            },
-            Delete(){
-                $('.check[check]').parent('li').detach()
-            }
+            ifDelete(){
+                // check是否至少选中一件商品
+                var isSelectedGood = this.products.some(function(item){
+                    return item.isChecked;
+                });
+                // 如果没有选中则弹框警告
+                if(!isSelectedGood){
+                    alert("请至少选择一件商品!");
+                } else {
+                    this.askDelete = true;
+                }
+		    }
             
+        },
+        watch:{
+            // var parent = document.getElementById("list_content")
+            // var child = parent.getElementsByTagName("*")
+            // console.log(child.length)
+        },
+        computed:{
+            // var parent = document.getElementById("list_content")
+            // var child = parent.getElementsByTagName("*")
+            // console.log(child.length)
+            totalPrice:function(){
+                var total = 0;
+                this.products.forEach(function(good){
+                    if(good.isChecked){
+                        total += good.qnPrimaryPrices * good.qnAmount;
+                    }
+                });
+                return total;
+                
+		    },
         },
         created:function(){
             // $.post('http://localhost:888/' +  'getProduct', {}, function(response){
@@ -138,8 +179,7 @@
                     // if(response.state){
 					// 	this.products.push(response.data);
 					// }
-					// console.log(response.data)
-                    this.products.push(response.data);
+                    this.products=response.data;
 				})
 
         }
